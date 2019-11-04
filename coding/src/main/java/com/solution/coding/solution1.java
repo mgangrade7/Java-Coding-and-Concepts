2e8c8c33ed9978cda729d3d4ec3283bb3a6cadb5
Mayank Gangrade <gangrademayank91@gmail.com>
2019-11-04 13:47:17 -0500

    initial commit

---
+++ coding/src/main/java/com/solution/coding/solution1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solution.coding;
+
+/**
+ *
+ * @author gangrade.m
+ */
+public class solution1 {
+    
+}
